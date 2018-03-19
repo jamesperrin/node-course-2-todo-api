60dacebdf67f5822a788a11f8e5a68568781f1f6
James Perrin <countrydawgg@gmail.com>
2018-03-19 08:33:20 -0700

    Add tests for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -9,8 +9,16 @@ const {
   Todo
 } = require('./../models/todo');
 
+const todos = [{
+  text: 'First test todo'
+}, {
+  text: 'Second test todo'
+}];
+
 beforeEach((done) => {
-  Todo.remove({}).then(() => done());
+  Todo.remove({}).then(() => {
+    return Todo.insertMany(todos);
+  }).then(() => done());
 });
 
 describe('POST /todos', () => {
@@ -32,9 +40,11 @@ describe('POST /todos', () => {
         if (err) {
           return done(err);
         }
-        
+
         // Check: length of Todos should EQ 1
-        Todo.find().then((todos) => {
+        Todo.find({
+          text
+        }).then((todos) => {
           expect(todos.length).toBe(1);
           expect(todos[0].text).toBe(text);
           done();
@@ -52,11 +62,22 @@ describe('POST /todos', () => {
         if (err) {
           return done(err);
         }
-       // Check: length of Todos should EQ 0
+        // Check: length of Todos should EQ 0
         Todo.find().then((todos) => {
-          expect(todos.length).toBe(0);
+          expect(todos.length).toBe(2);
           done();
         }).catch((e) => done(e));
       });
+  });
+});
+
+describe('GET /todos', () => {
+  it('should get all todos', (done) => {
+    request(app).get('/todos')
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todos.length).toBe(2)
+      })
+      .end(done);
   });
 });
